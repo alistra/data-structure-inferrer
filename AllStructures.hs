d0c8828dbbe91327a9bdb1263487a5e1d25c036f
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-04-22 14:59:20 +0200

    function to return all equally good structures

--- AllStructures.hs
+++ AllStructures.hs
@@ -92,3 +92,8 @@ recommendDS opns =  do  let sorted = reverse $ sortBy (\x y-> compareDS x y opns
                         let  bestStructures = head $ groupBy (\x y -> compareDS x y opns == EQ) sorted 
                         ridx <- randomRIO (0, length bestStructures - 1)
                         return $ bestStructures !! ridx
+
+recommendAllDS :: [OperationName] -> [Structure]
+recommendAllDS opns =  let sorted = reverse $ sortBy (\x y-> compareDS x y opns) allStructures 
+                        in  head $ groupBy (\x y -> compareDS x y opns == EQ) sorted 
+ 
