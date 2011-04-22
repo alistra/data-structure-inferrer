c731bf8c057d7614461fc905cdfb493dd4f46764
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-04-22 14:31:24 +0200

    todo notes, hlinted Structures

--- Structures.hs
+++ Structures.hs
@@ -1,6 +1,7 @@
 module Structures where
 
 import Data.List
+import Data.Ord
 
 data Structure = DS { getDSName :: String, getDSOps :: [DSOperation] }
 
@@ -19,12 +20,14 @@ data OperationName = InsertArgVal
                     | ExtremalVal
                     | BoundByArg
                     | BoundByVal
+                    | Union
+                    | Intersect
+                    | Difference
+                    | SymDifference
                     | Map
                     | Size
                     | Empty deriving Eq
                     
--- what with function that gets arg/val
-
 data ComplexityCharacteristics = AE | E | A | N deriving (Ord, Eq)
 
 type Complexity = (AsymptoticalComplexity, ComplexityCharacteristics)
@@ -39,14 +42,14 @@ instance Ord AsymptoticalComplexity where
 instance Show AsymptoticalComplexity where
     show (LinLog 0 0)   = "O(1)"
     show (LinLog 1 0)   = "O(n)"
-    show (LinLog 0 n)   = "O(" ++ (logs n) ++ " n)" 
-    show (LinLog 1 n)   = "O(n " ++ (logs n) ++ " n)" 
-    show (LinLog n 0)   = "O(n^" ++ (show n) ++ ")" 
-    show (LinLog n m)   = "O(n^" ++ (show n) ++ " " ++ (logs m) ++ " n)"
+    show (LinLog 0 n)   = "O("   ++ logs n ++ " n)" 
+    show (LinLog 1 n)   = "O(n " ++ logs n ++ " n)" 
+    show (LinLog n 0)   = "O(n^" ++ show n ++ ")" 
+    show (LinLog n m)   = "O(n^" ++ show n ++ " " ++ logs m ++ " n)"
 
 logs :: Integer -> String
 logs 0 = ""
-logs n = "log" ++ (logs (n-1))
+logs n = "log" ++ logs (n-1)
 
 
 data DSOperation = Op { getOpName :: OperationName, getComplexity :: Complexity } deriving Eq
@@ -56,20 +59,20 @@ instance Ord DSOperation where
 
 countElem :: Eq a => a -> [a] -> Integer
 countElem _ [] = 0
-countElem y (x:xs)  | y == x = 1 + (countElem y xs)
-                    | otherwise = (countElem y xs)
+countElem y (x:xs)  | y == x = 1 + countElem y xs
+                    | otherwise = countElem y xs
 
 compareDS ::  Structure -> Structure -> [OperationName] -> Ordering
-compareDS s1 s2 opns = let  ops1 = filter (\x -> elem (getOpName x) opns) (getDSOps s1)
-                            ops2 = filter (\x -> elem (getOpName x) opns) (getDSOps s2) in
-                                case compare (length ops1) (length ops2) of
+compareDS s1 s2 opns = let  ops1 = filter (\x -> getOpName x `elem` opns) (getDSOps s1)
+                            ops2 = filter (\x -> getOpName x `elem` opns) (getDSOps s2) in
+                                case Data.Ord.comparing length ops1 ops2 of
                                     LT -> LT
                                     GT -> GT
-                                    EQ -> if (length ops1) == 0
+                                    EQ -> if null ops1
                                         then EQ
-                                        else case compare (maximum ops1) (maximum ops2) of
+                                        else case Data.Ord.comparing maximum ops1 ops2 of
                                             LT -> GT
                                             GT -> LT
-                                            EQ -> let ordList = zipWith (compare) ops1 ops2 in
+                                            EQ -> let ordList = zipWith compare ops1 ops2 in
                                                 compare (countElem LT ordList) (countElem GT ordList)
 
