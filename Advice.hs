12e25ec9abefbad115b349a189ad462a06c3846c
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-05-07 02:26:30 +0200

    finally working advice

--- Advice.hs
+++ Advice.hs
@@ -16,27 +16,29 @@ better :: Structure -> Structure -> [OperationName] -> Bool
 better s1 s2 opns   | s1 == s2 = True
                     | otherwise = compareDS s1 s2 opns == GT
 
-adviceDS' :: Integer -> [OperationName] -> [Structure]
+betterThanEach :: Structure -> [Structure] -> [OperationName] -> Bool
+betterThanEach s1 ss opns = all (\s2-> better s1 s2 opns) ss 
+
+adviceDS' :: Integer -> [OperationName] -> [([OperationName], Structure)]
 adviceDS' n opns =  let recOrig = recommendAllDs opns
                         opnsSeqs = sequencesOfLen opns (toEnum (length opns) - n)
-                        recSeqs = nub $ concatMap (\seq-> filter (\ds-> all (\rds-> better ds rds seq) recOrig) 
-                                                            (recommendAllDs seq)) opnsSeqs 
-                            in recSeqs \\ recOrig
+                        recSeqs = concatMap (\seq -> map (\x -> (seq,x)) $ filter (\ds-> betterThanEach ds recOrig seq) (recommendAllDs seq)) opnsSeqs 
+                            in recSeqs -- \\ recOrig
 
 
-adviceDS :: [OperationName] -> [Structure]
-adviceDS = adviceDS' 1
+--adviceDS :: [OperationName] -> [Structure]
+--adviceDS = adviceDS' 1
 
-printAdvice' :: Integer -> [OperationName] -> IO () --add the colors of the rainbow
-printAdvice' n opns =   let adv = adviceDS' n opns 
-                            in mapM_ (printAdviceStructure opns) adv
+--printAdvice' :: Integer -> [OperationName] -> IO () --add the colors of the rainbow
+--printAdvice' n opns =   let adv = adviceDS' n opns 
+--                            in mapM_ (printAdviceStructure opns) adv
 
-printAdviceStructure :: [OperationName] -> Structure -> IO() --change to worse operations not only not existing ones
-printAdviceStructure opns s = putStr $    "You could use " ++
-                                            getDSName s ++
-                                            ", if you removed the following operations:\n" ++ 
-                                            concatMap (\opn -> "* " ++ show opn ++ "\n") (opns \\ map getOpName (getDSOps s))
+--printAdviceStructure :: [OperationName] -> Structure -> IO() --change to worse operations not only not existing ones
+--printAdviceStructure opns s = putStr $    "You could use " ++
+--                                            getDSName s ++
+--                                           ", if you removed the following operations:\n" ++ 
+--                                            concatMap (\opn -> "* " ++ show opn ++ "\n") (opns \\ map getOpName (getDSOps s))
                                     
 
-printAdvice :: [OperationName] -> IO ()
-printAdvice = printAdvice' 1
+--printAdvice :: [OperationName] -> IO ()
+--printAdvice = printAdvice' 1
