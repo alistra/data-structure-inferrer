d3c7df45df4ab2ff242931bb1ccdc2bfd4355aef
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-06-02 19:14:45 +0200

    dsu generation with existence of a ds, test framework

--- Analyzer.hs
+++ Analyzer.hs
@@ -4,6 +4,7 @@ import Defs.Structures
 import Defs.AST
 import Data.List
 import Control.Arrow
+import Control.Monad.State
 
 data DSInfo =   DSI { getDSIVarName :: Name,
                     isStatic :: Bool,
@@ -11,25 +12,29 @@ data DSInfo =   DSI { getDSIVarName :: Name,
 
 data DSUse =    DSU {  getDSUOpName :: OperationName,
                     isHeavilyUsed :: Bool, 
-                    isUserDependent :: Bool }
-                | DSUInit deriving (Show, Eq)
+                    isUserDependent :: Bool } deriving (Show, Eq)
 
-isDeclaredStructure t dsis = t `elem` map getDSIVarName dsis 
+type AnalyzerOutput = [(Name, DSUse)]
+type Analyzer = State [Name] AnalyzerOutput
 
 setHeavyUsage (DSU opname _ ud) = DSU opname True ud
 setUserDependance (DSU opname hu _) = DSU opname hu True
 
 isStaticDSU _ = True -- stub
 
-analyze :: [Term] -> [DSInfo]
-analyze = generateDSI . generateDSU  
 
-generateDSI :: [(Name, DSUse)] -> [DSInfo]
+
+analyze :: [Term] -> [DSInfo]
+analyze = generateDSI . generateDSU 
+
+
+
+generateDSI :: AnalyzerOutput -> [DSInfo]
 generateDSI allDSU@((name, _):dsus) = let (sameName, otherNames) = partition (\x -> fst x == name) allDSU in
     generateSingleDSI sameName : generateDSI otherNames
 generateDSI [] = []
 
-generateSingleDSI :: [(Name, DSUse)] -> DSInfo
+generateSingleDSI :: AnalyzerOutput -> DSInfo
 generateSingleDSI allNDSU@((name, _):_) = DSI name static cleanDSU where
     mergeDSU allDSU@(dsu:dsus) = let (sameOp, otherOps) = partition (\x -> getDSUOpName x == getDSUOpName dsu) allDSU in
         mergeSingleDSU sameOp : mergeDSU otherOps
@@ -37,20 +42,67 @@ generateSingleDSI allNDSU@((name, _):_) = DSI name static cleanDSU where
     cleanDSU = mergeDSU (map snd allNDSU)
     static = isStaticDSU cleanDSU
 
-generateDSU :: [Term] -> [(Name, DSUse)]
-generateDSU = concatMap stepDSU 
 
-stepDSU :: Term -> [(Name, DSUse)]
-stepDSU (Block body) = generateDSU body
-stepDSU (DSInit name) = [(name, DSUInit)]
-stepDSU (While cond body) = map (Control.Arrow.second setHeavyUsage) $ generateDSU [cond,body]
-stepDSU (Funcall name args) = case head args of
-    Var varname -> [(varname, DSU opname False False)] where
-        opname = case name of
-            "insert"        -> InsertVal
-            "max"           -> ExtremalVal
-            "update"        -> UpdateByVal
-            "delete_max"    -> DeleteExtremalVal
-    _ -> []
-stepDSU (If cond t1 t2) = generateDSU [cond,t1,t2]
-stepDSU _ = []
+
+generateDSU :: [Term] -> AnalyzerOutput
+generateDSU ts = evalState (foldlDSU stepDSU [] ts) []
+
+generateContextDSU :: [Term] -> Analyzer
+generateContextDSU = foldlDSU stepDSU []
+
+foldlDSU :: (a -> b -> State s a) -> a -> [b] -> State s a
+foldlDSU f start [] = return start
+foldlDSU f start (r:rest) = do 
+    dsus <- f start r
+    foldlDSU f dsus rest
+
+stepDSU :: AnalyzerOutput -> Term -> Analyzer
+
+stepDSU dsus (Block body) = do
+    newDSU <- generateContextDSU body
+    return $ dsus ++ newDSU
+
+stepDSU dsus (DSInit name) = do
+    ctx <- get
+    if name `elem` ctx 
+        then error $ name ++ " already initialized"
+        else put (name:ctx) >> return dsus 
+
+stepDSU dsus (While cond body) = do
+    newDSU <- generateContextDSU [cond,body] 
+    return $ dsus ++ map (Control.Arrow.second setHeavyUsage) newDSU
+
+stepDSU dsus (Funcall name args) = do
+    let opname = case name of
+            "insert"        -> Just InsertVal
+            "max"           -> Just ExtremalVal
+            "update"        -> Just UpdateByVal
+            "delete_max"    -> Just DeleteExtremalVal
+            _               -> Nothing
+
+    case opname of
+            Nothing ->  return dsus
+            Just op ->  case head args of
+                            Var varname -> do
+                                ctx <- get
+                                if varname `elem` ctx 
+                                    then return $ dsus ++ [(varname, DSU op False False)]
+                                    else error $ varname ++ " not initialized before use in function " ++ name
+                            _           -> error "Not implemented yet"
+
+stepDSU dsus (If cond t1 t2) = do
+    dsuCond <- generateContextDSU [cond]
+    oldCtx <- get
+
+    dsuT1 <- generateContextDSU [t1]
+    ctxT1 <- get
+
+    put oldCtx
+
+    dsuT2 <- generateContextDSU  [t2]
+    ctxT2 <- get
+
+    put $ union ctxT1 ctxT2
+    return $ dsus ++ concat [dsuCond, dsuT1, dsuT2]
+
+stepDSU dsus _ = return dsus
--- Tests.hs
+++ Tests.hs
@@ -1,6 +1,23 @@
 import Analyzer
 import Il.Lexer
 import Il.Parser
+import System.IO
 import Prelude hiding (lex)
 
+openTestFile name =
+    do catch (openFile name ReadMode)
+             (\_ -> do error $ "Cannot open "++ name)
+         
+runTestFiles :: [String] -> IO()
+runTestFiles = mapM_ runTest
+
+runTest :: String -> IO()
+runTest name = do
+    handle <- openTestFile name
+    contents <- hGetContents handle
+    hClose handle
+    putStrLn . show . test $ contents
+
 test = analyze.parse.lex
+
+runIlTests = runTestFiles $ map (\s -> "Il/tests/" ++ s ++ ".il") (map show [1..2])
