b1820b348d9ed9d35353bea5bb9e37e285fade27
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-06-16 12:03:47 +0200

    typechecker started

---
+++ Typechecker.hs
@@ -0,0 +1,39 @@
+module Typechecker where
+
+import Defs.Common
+import Defs.AST
+import Control.Monad.State
+{-
+data Type = TInt | TBool | TDs | TDsElem
+
+type Context = [(Name, Type)]
+type Typechecker a = State Context a
+
+typecheckP :: [Term] -> Bool
+
+
+
+typecheckT (And t1 t2) = 
+typecheckT (Assign n t1) =
+typecheckT (Block ts) =
+typecheckT (Dec n) =
+typecheckT (Div t1 t2) =
+typecheckT (DSInit n) =
+typecheckT (Eq t1 t2) =
+typecheckT (For t1 t2 t3 t4) =
+typecheckT (Funcall n ts) =
+typecheckT (Geq t1 t2) =
+typecheckT (Gt t1 t2) =
+typecheckT (If t1 t2 t3) =
+typecheckT (Inc n) =
+typecheckT (Int Int) =
+typecheckT (Leq t1 t2) =
+typecheckT (Lt t1 t2) =
+typecheckT (Mul t1 t2) =
+typecheckT (Not t1) =
+typecheckT (Or t1 t2) =
+typecheckT (Sub t1 t2) =
+typecheckT (Sum t1 t2) =
+typecheckT (While t1 t2) =
+typecheckT (Var n) =
+-}
