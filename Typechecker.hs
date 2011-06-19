5467ebedc02b8a64e0a943f2e93cd9c08f8a7c7b
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-06-19 16:33:10 +0200

    typechecker without functions, blocks and execState

--- Typechecker.hs
+++ Typechecker.hs
@@ -3,34 +3,153 @@ module Typechecker where
 import Defs.Common
 import Defs.AST
 import Control.Monad.State
-{-
 type TypeContext = [(Name, Type)]
 type Typechecker a = State TypeContext a
 
+{-
 typecheckP :: [Term] -> Bool
+typecheckP ts = evalState (mapM typecheckT ts) [] -- check is there a Just element
 
-typecheckT (And t1 t2) = 
-typecheckT (Assign n t1) =
-typecheckT (Block ts) =
-typecheckT (Dec n) =
-typecheckT (Div t1 t2) =
-typecheckT (Eq t1 t2) =
-typecheckT (For t1 t2 t3 t4) =
-typecheckT (Funcall n ts) =
-typecheckT (Geq t1 t2) =
-typecheckT (Gt t1 t2) =
-typecheckT (If t1 t2 t3) =
-typecheckT (Inc n) =
-typecheckT (Int n) =
-typecheckT (InitAssign n t tp) = 
-typecheckT (Leq t1 t2) =
-typecheckT (Lt t1 t2) =
-typecheckT (Mul t1 t2) =
-typecheckT (Not t1) =
-typecheckT (Or t1 t2) =
-typecheckT (Sub t1 t2) =
-typecheckT (Sum t1 t2) =
-typecheckT (While t1 t2) =
-typecheckT (Var n) =
-typecheckT (VarInit n tp) =
+typecheckB :: [Term] -> [Maybe Type]
+typecheckB = mapM typecheckT
 -}
+
+assertType t1 tp2 = do
+    tp1' <- typecheckT t1
+    case tp1' of
+        Just tp1 ->  if tp1 /= tp2
+            then error $ "Type error: " ++ (show t1) ++ " is type " ++ (show tp1) ++ ", should be" ++ (show tp2)
+            else return ()
+        Nothing -> error $ "Type error: " ++ (show t1) ++ " does not return a value, should return " ++ (show tp2)
+
+typecheckT :: Term -> Typechecker (Maybe Type)
+typecheckT (And t1 t2) = do
+    assertType t1 TBool
+    assertType t2 TBool
+    return $ Just TBool
+
+typecheckT (Assign v t1) = do
+    tcx <- get 
+    case lookup v tcx of
+        Just tp -> do
+            assertType t1 tp
+            return Nothing
+        Nothing -> error $ "Variable " ++ v ++ " not initialized"
+
+typecheckT (Block ts) = do
+--  typecheckB ts
+    return Nothing
+
+typecheckT (Dec v) = do
+    tcx <- get
+    case lookup v tcx of
+        Just TInt -> return $ Just TInt
+        Just tp | tp /= TInt -> error $ "Type error: " ++ v ++ " has type " ++ (show tp) ++ "instead of " ++ (show TInt)
+        Nothing -> error $ "Type error: " ++ v ++ " is not initilized, should be initialized as " ++ (show TInt)
+
+typecheckT (Div t1 t2) = do
+    assertType t1 TInt
+    assertType t2 TInt
+    return $ Just TInt
+
+typecheckT (Eq t1 t2) = do
+    assertType t1 TInt
+    assertType t2 TInt
+    return $ Just TBool
+
+typecheckT (For t1 t2 t3 t4) = do
+    typecheckT t1
+    assertType t2 TBool
+    typecheckT t3
+    typecheckT t4
+    return Nothing
+{-
+typecheckT (Funcall f ts) =
+-}
+typecheckT (Geq t1 t2) = do
+    assertType t1 TInt
+    assertType t2 TInt
+    return $ Just TBool
+
+typecheckT (Gt t1 t2) = do
+    assertType t1 TInt
+    assertType t2 TInt
+    return $ Just TBool
+
+typecheckT (If t1 t2 t3) = do
+    assertType t1 TBool
+    typecheckT t2
+    typecheckT t3
+    return Nothing
+
+typecheckT (Inc v) = do
+    tcx <- get
+    case lookup v tcx of
+        Just TInt -> return $ Just TInt
+        Just tp | tp /= TInt -> error $ "Type error: " ++ v ++ " has type " ++ (show tp) ++ "instead of " ++ (show TInt)
+        Nothing -> error $ "Type error: " ++ v ++ " is not initilized, should be initialized as " ++ (show TInt)
+
+typecheckT (Int n) = return $ Just TInt
+
+typecheckT (InitAssign v t tp) = do
+    assertType t tp
+    tcx <- get 
+    case lookup v tcx of
+        Just tp1 -> error $ "Variable " ++ v ++ " already initialized with type " ++ (show tp1)
+        Nothing -> do
+            put $ (v,tp):tcx
+            return Nothing
+
+typecheckT (Leq t1 t2) = do
+    assertType t1 TInt
+    assertType t2 TInt
+    return $ Just TBool
+
+typecheckT (Lt t1 t2) = do
+    assertType t1 TInt
+    assertType t2 TInt
+    return $ Just TBool
+
+typecheckT (Mul t1 t2) = do
+    assertType t1 TInt
+    assertType t2 TInt
+    return $ Just TInt
+
+typecheckT (Not t1) = do
+    assertType t1 TBool
+    return $ Just TBool
+
+typecheckT (Or t1 t2) = do
+    assertType t1 TBool
+    assertType t2 TBool
+    return $ Just TBool
+
+typecheckT (Sub t1 t2) = do
+    assertType t1 TInt
+    assertType t2 TInt
+    return $ Just TInt
+
+typecheckT (Sum t1 t2) = do
+    assertType t1 TInt
+    assertType t2 TInt
+    return $ Just TInt
+
+typecheckT (While t1 t2) = do
+    assertType t1 TBool
+    typecheckT t2
+    return Nothing
+
+typecheckT (Var v) = do
+    tcx <- get 
+    case lookup v tcx of
+        Nothing -> error $ "Variable " ++ v ++ " not initialized"
+        Just tp -> return $ Just tp
+    
+typecheckT (VarInit v tp) = do
+    tcx <- get 
+    case lookup v tcx of
+        Just tp -> error $ "Variable " ++ v ++ " already initialized with type " ++ (show tp)
+        Nothing -> do
+            put $ (v,tp):tcx
+            return Nothing
+    
