fe3050cb3dc9dcc5f2cca0964fa605cb0aec71bb
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-09-30 01:27:12 +0200

    trspace

--- Recommend.hs
+++ Recommend.hs
@@ -1,5 +1,5 @@
 -- | Module for recommending a data structure based on operations used
-module Recommend 
+module Recommend
   ( recommendDS,
     recommendAllDs ) where
 
@@ -12,8 +12,8 @@ import Random
 
 -- | Recommends a data structure which is best for given operations @opns@. If there's more than one optimal structure, it chooses one at random
 recommendDS :: [OperationName] -> IO Structure
-recommendDS opns =  do  let sorted = reverse $ sortBy (\x y-> compareDS x y opns) allStructures 
-                        let bestStructures = head $ groupBy (\x y -> compareDS x y opns == EQ) sorted 
+recommendDS opns =  do  let sorted = reverse $ sortBy (\x y-> compareDS x y opns) allStructures
+                        let bestStructures = head $ groupBy (\x y -> compareDS x y opns == EQ) sorted
                         ridx <- randomRIO (0, length bestStructures - 1)
                         return $ bestStructures !! ridx
 
@@ -24,4 +24,4 @@ recommendAllDs opns = recommendAllDsFromList opns allStructures
 -- | Recommends the best possible data structure from @structs@ for given operations @opns@
 recommendAllDsFromList :: [OperationName] -> [Structure] -> [Structure]
 recommendAllDsFromList opns structs =    let sorted = reverse $ sortBy (\x y-> compareDS x y opns) structs
-                                            in head $ groupBy (\x y -> compareDS x y opns == EQ) sorted 
+                                            in head $ groupBy (\x y -> compareDS x y opns == EQ) sorted
