e9370183031464bfdff54018c401e080fdb26b4a
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-09-29 21:21:42 +0200

    trailing spaces

--- Analyzer.hs
+++ Analyzer.hs
@@ -15,14 +15,14 @@ import Data.List
 import Control.Monad.State
 
 -- | Data structure for analysis info
-data DSInfo = DSI { 
+data DSInfo = DSI {
     getDSINames  :: [(FunctionName, VariableName)],  -- ^ Variable holding the data structure --FIXME pointer copying
     getDSIDSU      :: [DSUse]                          -- ^ Data structure use cases
     } deriving (Show, Eq)
 
 -- | Data structure for function info
-data DSFun = DSF { 
-    getDSFFun   :: Function,      
+data DSFun = DSF {
+    getDSFFun   :: Function,
     getDSFCalls :: [(FunctionName, [Maybe VariableName])],
     getDSFDSI   :: [DSInfo]
     } deriving (Show, Eq)
@@ -34,7 +34,7 @@ data DSUse = DSU {
     isUserDependent :: Bool             -- ^ Is it dependent on some external input (user, network, random, signals, etc.)
     } deriving (Show, Eq)
 
--- | State monad with 'AnalyzerState' 
+-- | State monad with 'AnalyzerState'
 type Analyzer a = State AnalyzerState a
 
 type AnalyzerOutput = [(VariableName, DSUse)]
@@ -51,63 +51,63 @@ data AnalyzerState = AS {
 setHeavyUsage (DSU opname _ ud) = DSU opname True ud
 setUserDependance (DSU opname hu _) = DSU opname hu True
 
--- | Pretty print single 'DSInfo' 
+-- | Pretty print single 'DSInfo'
 printDSI :: DSInfo -> IO()
 printDSI dsi = do
     putStr "The recommended structure for "
     redColor
     print $ getDSINames dsi
-    resetColor 
+    resetColor
     putStrLn " is:"
     cyanColor
     recommendedDS >>= print
     resetColor where
-        recommendedDS = do 
+        recommendedDS = do
             let opns = map getDSUName $ getDSIDSU dsi
             recommendDS opns
 
 -- | Pretty printer for the analyzer effects
 printRecommendationFromAnalysis :: [DSInfo] -> IO()
-printRecommendationFromAnalysis = mapM_ printDSI 
+printRecommendationFromAnalysis = mapM_ printDSI
 
 -- | Runs everything that is needed to analyze a program
 analyze :: [Function] -> [DSInfo]
 analyze functions = let functionNames = map getFunName functions in
     let dsfs = map (generateDSF functionNames) functions in
     closeDSIs dsfs
-    
+
 -- | Merges the simple 'DSInfo's based on function calls from the functions
 closeDSIs :: [DSFun] -> [DSInfo]
 closeDSIs dsfs = let startingDSF = lookupFun startingFunction in
-    let dsiVars = map snd $ filter (\(fn,vn) -> fn == startingFunction) $ concatMap getDSINames $ getDSFDSI startingDSF in 
+    let dsiVars = map snd $ filter (\(fn,vn) -> fn == startingFunction) $ concatMap getDSINames $ getDSFDSI startingDSF in
     concatMap (\var -> closeDSIs' startingDSF var []) dsiVars where
 
         closeDSIs' :: DSFun -> VariableName -> [FunctionName] -> [DSInfo]
         closeDSIs' dsf var accu = let funname = getFunName (getDSFFun dsf) in
-            if funname `elem` accu 
+            if funname `elem` accu
                 then []
                 else let funcalls = getDSFCalls dsf in
-                    let varConts = concatMap bindFuncall funcalls in 
+                    let varConts = concatMap bindFuncall funcalls in
                     let dsis = getDSFDSI dsf in
                     let currDSI = lookupDSI dsis funname var in
                     let otherDSI = dsis \\ [currDSI] in
                     (foldl1 mergeDSI $ currDSI:(concatMap (\(fn, vn) -> (closeDSIs' (lookupFun fn) vn (funname:accu))) varConts)):otherDSI where
 
                         bindFuncall :: (FunctionName, [Maybe VariableName]) -> [(VariableName, VariableName)]
-                        bindFuncall  = bindFuncall' 1 
+                        bindFuncall  = bindFuncall' 1
 
                         bindFuncall' :: Int -> (FunctionName, [Maybe VariableName]) -> [(FunctionName, VariableName)]
-                        bindFuncall' n (fn, Just vn:vns) = if vn == var 
+                        bindFuncall' n (fn, Just vn:vns) = if vn == var
                             then (fn, getNewVarName fn n): bindFuncall' (n+1) (fn, vns)
                             else bindFuncall' (n+1) (fn, vns)
                         bindFuncall' n (fn, Nothing:vns) = bindFuncall' (n+1) (fn, vns)
                         bindFuncall' n (fn, []) = []
 
 
-                        mergeDSI :: DSInfo -> DSInfo -> DSInfo 
+                        mergeDSI :: DSInfo -> DSInfo -> DSInfo
                         mergeDSI (DSI n1 d1) (DSI n2 d2) = DSI (n1++n2) (nub $ d1++d2)
-        
-                        lookupDSI :: [DSInfo] -> FunctionName -> VariableName -> DSInfo 
+
+                        lookupDSI :: [DSInfo] -> FunctionName -> VariableName -> DSInfo
                         lookupDSI dsis funname var = let goodDSI = filter (\dsi -> (funname, var) `elem` getDSINames dsi) dsis in
                             if length goodDSI /= 1
                                 then error $ "None or too many matching DSI " ++ show (funname, var)
@@ -131,7 +131,7 @@ generateDSI fn dsus = let varGroups = groupBy (\(varname1,_) (varname2,_) -> var
 
 -- | Start the state monad to create a 'DSFun' for function
 generateDSF :: [FunctionName] -> Function -> DSFun
-generateDSF fnns fn = let (dsus, st) = runState (foldlTerms step [] [getFunBody fn]) (AS fn fnns [] []) in 
+generateDSF fnns fn = let (dsus, st) = runState (foldlTerms step [] [getFunBody fn]) (AS fn fnns [] []) in
     DSF fn (getStateCalls st) (generateDSI fn dsus)
 
 -- | Analyze a block of terms using the state monad
@@ -141,14 +141,14 @@ stepBlock = foldlTerms step [] where
 -- | Foldl 'Term's using the 'step' function to generate 'AnalyzerOutput'
 foldlTerms :: (AnalyzerOutput -> Term -> Analyzer AnalyzerOutput) -> AnalyzerOutput -> [Term] -> Analyzer AnalyzerOutput
 foldlTerms f start [] = return start
-foldlTerms f start (r:rest) = do 
+foldlTerms f start (r:rest) = do
     dsus <- f start r
     foldlTerms f dsus rest
 
 -- | Function putting a variable definition in the context
 putVar :: VariableName -> Analyzer ()
 putVar name = do
-    state <- get 
+    state <- get
     put $ AS (getStateFunction state) (getStateFunNames state) (name:getStateVarNames state) (getStateCalls state)
 
 -- | Function returning 'True' if the variable is already defined
@@ -156,11 +156,11 @@ getVar :: VariableName -> AnalyzerState -> Bool
 getVar name state = name `elem` getStateVarNames state
 
 -- | Function putting a function call in the state
-putCall :: FunctionName -> [Term] -> Analyzer () 
+putCall :: FunctionName -> [Term] -> Analyzer ()
 putCall name args = do
     state <- get
     let cleanArgs = map justifyVars args
-    let call = (name, cleanArgs) 
+    let call = (name, cleanArgs)
     put $ AS (getStateFunction state) (getStateFunNames state) (getStateVarNames state) (call:getStateCalls state) where
         justifyVars :: Term -> Maybe VariableName
         justifyVars (Var v) = Just v -- FIXME should work on function calls returning dses, not only vars
@@ -177,7 +177,7 @@ step dsus (VarInit name Ds) = do
     s <- get
     if getVar name s
         then error $ name ++ " already initialized"
-        else putVar name >> return dsus 
+        else putVar name >> return dsus
 
 step dsus (VarInit name _) = return dsus
 
@@ -185,23 +185,23 @@ step dsus (InitAssign name term Ds) = do
     s <- get
     if getVar name s
         then error $ name ++ " already initialized"
-        else putVar name >> return dsus 
+        else putVar name >> return dsus
 
 step dsus (InitAssign name term _) = return dsus
 
 step dsus (While cond body) = do
-    newDSU <- stepBlock [cond,body] 
+    newDSU <- stepBlock [cond,body]
     return $ dsus ++ map (\(b,c) -> (b,setHeavyUsage c)) newDSU -- FIXME smarter heavy load recognition
 
-step dsus (Funcall name args) = do 
-    s <- get 
+step dsus (Funcall name args) = do
+    s <- get
     let opname = case name of -- FIXME nicer with usage of dsinfFunctions from Common
             "insert"        -> Just InsertVal
             "find"          -> Just FindByVal
-            "update"        -> Just UpdateByRef 
+            "update"        -> Just UpdateByRef
             "max"           -> Just ExtremalVal
             "delete_max"    -> Just DeleteExtremalVal
-            _               -> Nothing 
+            _               -> Nothing
                                             -- FIXME add reading the function calls
     argDsus <- stepBlock args
 
@@ -219,15 +219,15 @@ step dsus (Funcall name args) = do
 
 step dsus (If cond t1 t2) = do
     dsuCond <- stepBlock [cond]
-    oldState <- get 
+    oldState <- get
 
     dsuT1 <- stepBlock [t1]
-    stateT1 <- get 
+    stateT1 <- get
 
     put oldState
 
     dsuT2 <- stepBlock  [t2]
-    stateT2 <- get 
+    stateT2 <- get
 
     put $ AS (getStateFunction stateT1) (getStateFunNames stateT1) (getStateVarNames stateT1 `union` getStateVarNames stateT2) (getStateCalls stateT1 `union` getStateCalls stateT2)
     return $ dsus ++ concat [dsuCond, dsuT1, dsuT2]
