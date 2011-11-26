a6444800209bdc515af46d09afd444112826b103
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-11-26 20:49:16 +0100

    canalyzer stuff

--- CAnalyzer.hs
+++ CAnalyzer.hs
@@ -3,7 +3,7 @@ module CAnalyzer where
 import Language.C
 import Language.C.System.GCC
 
-main = parseMyFile "1.c" >>= (return . analyzeProgram) >>= print
+main = parseMyFile "1.c" >>=  analyzeProgram
 
 parseMyFile :: FilePath -> IO CTranslUnit
 parseMyFile input_file =
@@ -12,13 +12,35 @@ parseMyFile input_file =
        Left parse_err -> error (show parse_err)
        Right ast      -> return ast
 
-analyzeProgram = prettyUsingInclude
-{-
-analyzeProgram (CTranslUnit extDecls _) = mapM_ analyzeExtDecl extDecls
+--analyzeProgram = prettyUsingInclude
+analyzeProgram a@(CTranslUnit extDecls _) = putStrLn "Analyzing Translation Unit" >> mapM_ analyzeExtDecl extDecls
 
-analyzeExtDecl (CDeclExt (CDecl decls _ _)) = print decls
-analyzeExtDecl (CFDefExt (CFunDef tp decl _ _ _)) = print "fundef!"
--}
+analyzeExtDecl a@(CDeclExt cdecl) = putStrLn "Analyzing CDeclExt" >> analyzeCDecl cdecl
+analyzeExtDecl a@(CFDefExt cfundef) = putStrLn "Analyzing CFDefExt" >> analyzeCFunDef cfundef
+analyzeExtDecl a@(CAsmExt strLit dunno) = putStrLn "Analyzing CAsmExt" >> print a
+
+analyzeCDecl a@(CDecl specifiers declList _) = do
+    putStrLn "Analyzing CDecl"
+    analyzeCDeclSpecifiers specifiers
+    analyzeCDeclDeclList declList
+
+analyzeCDeclSpecifiers specifiers = print "specifiers"
+
+analyzeCDeclDeclList declList = print "decl list"
+
+analyzeCFunDef (CFunDef specifiers declarator declaration statement _) = do
+    putStrLn "Analyzing CFunDef"
+    analyzeCDeclSpecifiers specifiers
+    analyzeCDeclarator declarator
+    analyzeCDeclaration declaration
+    analyzeCStatement statement
+
+analyzeCDeclarator declarator = putStrLn "Analyzing CDeclarator" >> print declarator
+
+analyzeCDeclaration declaration = putStrLn "Analyzing CDeclaration" >> print declaration
+
+analyzeCStatement statement = putStrLn "Analyzing CStatement" >> print statement
+
 {-
 Right (CTranslUnit
 [ CDeclExt (CDecl [CTypeSpec (CSUType (CStruct CStructTag (Just "ds") (Just []) [] ) )] [] )
