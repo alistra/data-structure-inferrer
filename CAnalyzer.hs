982d557c5eab22f005f7a05a768459c890364a75
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-11-29 15:32:04 +0100

    canal

--- CAnalyzer.hs
+++ CAnalyzer.hs
@@ -39,9 +39,62 @@ analyzeCDeclarator declarator = putStrLn "Analyzing CDeclarator" >> print declar
 
 analyzeCDeclaration declaration = putStrLn "Analyzing CDeclaration" >> print declaration
 
-analyzeCStatement statement = putStrLn "Analyzing CStatement" >> print statement
+analyzeCStatement (CLabel ident statement attrib _) = do
+    putStrLn "Analyzing CStatement"
+    print "label"
+    analyzeCStatement statement
 
-{-
+analyzeCStatement (CCase expr statement _) = putStrLn "Analyzing CStatement" >> print "case" >> analyzeCStatement statement
+analyzeCStatement (CCases expr1 expr2 statement _)          = putStrLn "Analyzing CStatement" >> print "cases"
+analyzeCStatement (CDefault statement _)                    = putStrLn "Analyzing CStatement" >> print "default"
+analyzeCStatement (CExpr mexpr _)                           = putStrLn "Analyzing CStatement" >> print "expr"
+analyzeCStatement (CCompound idents compoundBlockItems _) = do
+    putStrLn "Analyzing CStatement"
+    print idents
+    mapM_ analyzeCCompoundBlockItem compoundBlockItems
+
+analyzeCStatement (CIf expr statement mstatement _)         = putStrLn "Analyzing CStatement" >> print "if"
+analyzeCStatement (CSwitch expr statement _)                = putStrLn "Analyzing CStatement" >> print "switch"
+analyzeCStatement (CWhile expr statement bool _)            = putStrLn "Analyzing CStatement" >> print "while"
+analyzeCStatement (CFor either mexpr1 mexpr2 statement _)   = putStrLn "Analyzing CStatement" >> print "for"
+analyzeCStatement (CGoto ident _)                           = putStrLn "Analyzing CStatement" >> print "goto"
+analyzeCStatement (CGotoPtr expr _)                         = putStrLn "Analyzing CStatement" >> print "gotoPtr"
+analyzeCStatement (CCont _)                                 = putStrLn "Analyzing CStatement" >> print "continue"
+analyzeCStatement (CBreak _)                                = putStrLn "Analyzing CStatement" >> print "break"
+analyzeCStatement (CReturn mexpr _)                         = putStrLn "Analyzing CStatement" >> print "return"
+analyzeCStatement (CAsm asm _)                              = putStrLn "Analyzing CStatement" >> print "asm"
+
+analyzeCCompoundBlockItem (CBlockStmt statement)            = putStrLn "Analyzing CBlockStmt" >> analyzeCStatement statement
+analyzeCCompoundBlockItem (CBlockDecl declaration)          = putStrLn "Analyzing CBlockDecl" >> analyzeCDecl declaration
+analyzeCCompoundBlockItem (CNestedFunDef funDef)            = putStrLn "Analyzing CNestedFunDef" >> print funDef
+
+{-analyzeCExpression (CComma exprs _) = putStrLn "Analyzing CExpression" >> putStrLn "Analyzing CComma" >> mapM_ analyzeCExpression exprs
+CAssign CAssignOp (CExpression a) (CExpression a) a  
+CCond (CExpression a) (Maybe (CExpression a)) (CExpression a) a  
+CBinary CBinaryOp (CExpression a) (CExpression a) a  
+CCast (CDeclaration a) (CExpression a) a     
+CUnary CUnaryOp (CExpression a) a    
+CSizeofExpr (CExpression a) a    
+CSizeofType (CDeclaration a) a   
+CAlignofExpr (CExpression a) a   
+CAlignofType (CDeclaration a) a  
+CComplexReal (CExpression a) a   
+CComplexImag (CExpression a) a   
+CIndex (CExpression a) (CExpression a) a     
+CCall (CExpression a) [CExpression a] a  
+CMember (CExpression a) Ident Bool a     
+CVar Ident a     
+CConst (CConstant a)    
+integer, character, floating point and string constants
+CCompoundLit (CDeclaration a) (CInitializerList a) a    
+C99 compound literal
+CStatExpr (CStatement a) a  
+GNU C compound statement as expr
+CLabAddrExpr Ident a    
+GNU C address of label
+CBuiltinExpr (CBuiltinThing a)
+
+
 Right (CTranslUnit
 [ CDeclExt (CDecl [CTypeSpec (CSUType (CStruct CStructTag (Just "ds") (Just []) [] ) )] [] )
 , CDeclExt (CDecl [CStorageSpec (CExtern ),CTypeSpec (CVoidType )] [(Just (CDeclr (Just "insert") [CFunDeclr (Right ([CDecl [CTypeSpec (CSUType (CStruct CStructTag (Just "ds") Nothing [] ) )] [(Just (CDeclr Nothing [CPtrDeclr [] ] Nothing [] (OnlyPos <no file> (<no file>,-1))),Nothing,Nothing)] ,CDecl [CTypeSpec (CIntType )] [] ],False)) [] ] Nothing [] ),Nothing,Nothing)] )
