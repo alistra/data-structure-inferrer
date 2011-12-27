9dddd1a9d781ec6d2db33c734bba2ffd1926ba2d
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-12-27 02:16:00 +0100

    tests syntax fix

--- C/tests/ins_sea_upd_max.c
+++ C/tests/ins_sea_upd_max.c
@@ -1,14 +1,15 @@
+typedef int dstype;
+#include "../../dsimp/ds.h"
 int main()
 {
 	ds d1;
-	dselem e;
 
 	for(int i = 0; i < 20; i++)
 	{
-		insert(d1, i);
-		e = search(d1, i);
-		update(e, 2*i);
+		insert_d(d1, i);
+		printf("%d\n", search_d(d1, i));
+		update_d(d1, i, 2*i);
 	}
 
-	printf("%d\n", max(d1));
+	printf("%d\n", max_d(d1));
 }
