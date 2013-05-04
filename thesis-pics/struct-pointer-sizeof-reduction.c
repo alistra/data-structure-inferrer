26cd830132d14e6b7081f00f19c8c716dd40f892
Aleksander Balicki <balicki.aleksander@gmail.com>
2013-05-04 14:07:31 +0200

    some work on weights

--- thesis-pics/struct-pointer-sizeof-reduction.c
+++ thesis-pics/struct-pointer-sizeof-reduction.c
@@ -1,13 +1,6 @@
-#define length (3 * sizeof(int) + 128)
-
-int cmp(char *p1, char *p2) {
-
-	return (int)p1 - (int)p2;
-}
-
 int main(int argc, char **argv) {
 
-	char p[length];
+	char p[sizeof(struct person)];
 	...
 	ds d;
 	d = dsinit(cmp);
