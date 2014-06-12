6ba93f4a291c5de86fa76a5aca62135ce27947b3
Aleksander Balicki <alistra@wikia-inc.com>
2014-06-11 21:21:24 -0700

    code example updated

--- thesis-pics/upper-bound-transform.c
+++ thesis-pics/upper-bound-transform.c
@@ -2,6 +2,17 @@
 
 int main(int argc, char ** argv)
 {
-	printf("Hello world!\n");
+
+	ds d = ds_init();
+	
+	for (int i = 0; i < 1024; i++)
+		insert_d(d, f(i));
+
+
+	g();
+	delete_d(d, 1337);
+	//part of the code not inserting any more elements
+
+
 	return 0;
 }
