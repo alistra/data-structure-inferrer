fd25eea91f525aa635612c5dc4be1fa71298adc5
Aleksander Balicki <balicki.aleksander@gmail.com>
2014-09-09 17:46:01 +0200

    last code sample, some work on linked structures

--- thesis-pics/minimal-elem-count.c
+++ thesis-pics/minimal-elem-count.c
@@ -2,6 +2,18 @@
 
 int main(int argc, char ** argv)
 {
-	printf("Hello world!\n");
+
+	ds d = ds_init();
+	
+	for (int i = 0; i < 100; i++)
+		insert_d(d, f(i));
+
+	delete_d(d, 1337);
+	
+	for (int i = 0; i < 10000; i++)
+		insert_d(d, f(i));
+
+	delete_max_d(d);
+
 	return 0;
 }
